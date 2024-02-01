8d44cf5c134d61465dafb17ebd45247a395aefe2
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-02-01 22:33:26 +0700

    config: install @vue/apollo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { createApp, h, provide } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://library-api.alab.in/graphql'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
 
 app.use(router)
 
